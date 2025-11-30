5688387e0bea2e0ff21622883113d61afac26294
Nathan FLATTIN <nathan.flattin@cryptio.co>
2025-11-30 23:15:55 +0900

    feat: test

--- backend/crates/api/src/main.rs
+++ backend/crates/api/src/main.rs
@@ -19,7 +19,7 @@ async fn main() -> Result<()> {
     let app = api::build(app_state, node_state);
     let listener = TcpListener::bind(address).await?;
 
-    info!("Listening ont {}", address);
+    info!("Listening on {}", address);
 
     // TODO make a .into_make_service?
     axum::serve(listener, app).await?;
